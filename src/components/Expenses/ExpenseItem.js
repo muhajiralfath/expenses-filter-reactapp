15b81119d721885c16d351691ec589e19f2fbb33
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-03-23 14:38:55 +0700

    Buat dinamic list item

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,20 @@
-import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const handleClick = () => {
-    setTitle("Updated!");
-  };
-
   return (
-    <Card className="expense-item">
-      <div>
-        <ExpenseDate date={props.date} />
-      </div>
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={handleClick}>Click</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <div>
+          <ExpenseDate date={props.date} />
+        </div>
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
 
